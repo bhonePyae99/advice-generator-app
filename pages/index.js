fb7a2a86502e33223c1cb91401c59dac6e10a68b
Bhone Pyae Kyaw <bhonepyaekyaw1899@gmail.com>
2022-02-24 12:42:31 +0630

    final work

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,28 @@
 import Head from "next/head";
 import Image from "next/image";
+import axios from "axios";
+import { useState } from "react";
+export const getServerSideProps = async () => {
+  const resp = await axios.get("https://api.adviceslip.com/advice");
+  return { props: { advice: resp.data } };
+};
 
-export default function Home() {
+export default function Home({ advice }) {
+  const [able, setAble] = useState(true);
+  const [randomAdvice, setRandomAdvice] = useState({
+    id: advice.slip.id,
+    advice: advice.slip.advice,
+  });
+
+  const updateAdvice = async () => {
+    const resp = await axios.get("https://api.adviceslip.com/advice");
+    const { slip } = resp.data;
+    setRandomAdvice({ id: slip.id, advice: slip.advice });
+    setAble(false);
+    setTimeout(() => {
+      setAble(true);
+    }, 2000);
+  };
   return (
     <div>
       <Head>
@@ -13,26 +34,17 @@ export default function Home() {
       <main>
         <div className="bg-grayishBlue rounded-xl md:w-[500px] w-[95%] h-auto p-7 absolute left-1/2 top-1/2 text-center -translate-x-1/2 -translate-y-1/2">
           <h4 className="uppercase text-neonGreen tracking-widest font-bold">
-            advice #117
+            advice #{randomAdvice.id}
           </h4>
-          <div className="text-2xl text-white font-bold py-7  border-b mb-10 relative">
-            <h2>"it is better to do the shit you want to do"</h2>
-            <svg
-              width="444"
-              className="absolute bottom-0 left-1/2 -translate-x-1/2 translate-y-1/2"
-              height="16"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g fill="none" fill-rule="evenodd">
-                <path fill="#4E5D73" d="M0 8h196v1H0zM248 8h196v1H248z" />
-                <g transform="translate(212)" fill="#CEE3E9">
-                  <rect width="6" height="16" rx="3" />
-                  <rect x="14" width="6" height="16" rx="3" />
-                </g>
-              </g>
-            </svg>
+          <div className="text-2xl text-white font-bold py-7  border-b-2 mb-10 relative">
+            <h2>"{randomAdvice.advice}"</h2>
           </div>
-          <div className="rounded-full bg-neonGreen flex justify-center items-center absolute left-1/2 shadow-white bright -translate-x-1/2 h-14 w-14">
+          <div
+            className={
+              "rounded-full bg-neonGreen flex justify-center cursor-pointer items-center absolute left-1/2 shadow-white bright -translate-x-1/2 h-14 w-14 "
+            }
+            onClick={updateAdvice}
+          >
             <svg width="24" height="24" xmlns="http://www.w3.org/2000/svg">
               <path
                 d="M20 0H4a4.005 4.005 0 0 0-4 4v16a4.005 4.005 0 0 0 4 4h16a4.005 4.005 0 0 0 4-4V4a4.005 4.005 0 0 0-4-4ZM7.5 18a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm4.5 4.5a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Zm0-9a1.5 1.5 0 1 1 0-3 1.5 1.5 0 0 1 0 3Z"
